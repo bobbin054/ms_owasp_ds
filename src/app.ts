67fa2d74298c12464144de572908b6d6e49fc2f4
Joakim Östelid <joakim.ostelid@outlook.com>
2023-11-17 11:02:03 +0100

    Refactor import statements and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import path from "path";
 import { spawn, exec } from "child_process";
-import * as util from "util";
-import * as fs from "fs/promises";
+import { promisify } from "util";
+import { readFile } from "fs/promises";
 
 const app = express();
 const port = 3000;
@@ -11,10 +11,7 @@ type RequestBody = {
   gitUrl: string;
 };
 
-const spawnAsync = util.promisify(spawn);
-const execAsync = util.promisify(exec);
-
-// Add this line to enable JSON parsing middleware
+const execAsync = promisify(exec);
 app.use(express.json());
 
 app.get("/", (_req, res) => {
@@ -23,33 +20,19 @@ app.get("/", (_req, res) => {
 
 app.post("/api/owasp-dependency-check", async (request, res) => {
   console.log(`Http request for url "${request.url}"`);
-
-  console.log(`Parsing request body as JSON`);
   console.log(`Request body: ${JSON.stringify(request.body, null, 2)}`);
-  // Parse the request body as JSON
-  const requestJson = request.body;
-  console.log(`Parsed request body: ${JSON.stringify(requestJson, null, 2)}`);
-
-  // Validate the input
-  if (!requestJson) {
+  if (!request.body) {
     res.status(400).send("Bad Request: Missing request body");
   }
-  // Set the OWASP Dependency-Check data directory
-  const dependencyCheckDataDir = process.env.DEPENDENCY_CHECK_DATA_DIR || "/opt/dependency-check/data";
-
-  const { gitUrl } = requestJson as RequestBody;
+  const { gitUrl } = request.body as RequestBody;
   console.log(`gitUrl: ${gitUrl}`);
-  // const tempDir = `/tmp/gitrepos/${context.invocationId}`;
-  const tempDir = "/tmp/gitrepos/bobbin054";
+  const tempDir = `/tmp/gitrepos/${request.ip}`;
   console.log(`tempDir: ${tempDir}`);
   const reportPath = path.join(tempDir, "dependency-check-report.json");
   console.log(`reportPath: ${reportPath}`);
-
   try {
-    const { stdout: gitCommandStdout, stderr: gitCommandStderr } = await execAsync(`git clone ${gitUrl} ${tempDir}`);
-
+    await execAsync(`git clone ${gitUrl} ${tempDir}`);
     console.log(`Running owasp dependency-check on: ${gitUrl}`);
-
     // Use spawn to run the command
     const dependencyCheckProcess = spawn("dependency-check.sh", [
       "--project",
@@ -61,7 +44,6 @@ app.post("/api/owasp-dependency-check", async (request, res) => {
       "--format",
       "JSON",
     ]);
-
     // Capture and stream stdout and stderr
     dependencyCheckProcess.stdout.on("data", (data) => {
       console.log(`Dependency Check stdout: ${data}`);
@@ -69,24 +51,20 @@ app.post("/api/owasp-dependency-check", async (request, res) => {
     dependencyCheckProcess.stderr.on("data", (data) => {
       console.error(`Dependency Check stderr: ${data}`);
     });
-
     // Wait for the process to exit
     const exitCode: number | null = await new Promise((resolve) => {
       dependencyCheckProcess.on("close", (code) => resolve(code));
     });
-
     if (exitCode !== 0) {
       console.error(`Dependency Check process exited with code ${exitCode}`);
       return res.status(500).send(`Dependency Check process exited with code ${exitCode}`);
     }
-
     // Read the JSON report
-    const report = await fs.readFile(reportPath, "utf8");
+    const report = await readFile(reportPath, "utf8");
     console.log(`Report: ${report}`);
-
     // Clean up
     execAsync(`rm -rf ${tempDir}`);
-
+    // Send the report as the response
     return res.status(201).contentType("application/json").send(report);
   } catch (err) {
     if (err instanceof Error) {
